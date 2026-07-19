0a91e575d4c343f31be29e25d60fb43e012d54da
Yadhukrishna PR <yadhukrishna.pr.61@gmail.com>
2026-07-19 04:35:37 +0000

    Containerized CloudCart application with Docker

---
+++ config/settings.py
@@ -0,0 +1,15 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+class Config:
+
+    SECRET_KEY = "cloudcart-devops-secret"
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
+        BASE_DIR,
+        "database",
+        "cloudcart.db"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
